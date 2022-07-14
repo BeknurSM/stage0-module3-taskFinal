90ad582d7a74a23a7b1430a1ca633a1947d18b16
BekoSm <b.smagulov.1999@gmail.com>
2022-07-14 16:05:09 +0600

    Push1

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,14 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int temp;
+        temp = number%10;
+        System.out.print(temp);
+        temp = number/10;
+        temp = temp%10;
+        System.out.print(temp);
+        temp = number/10;
+        temp = temp/10;
+        System.out.print(temp);
     }
 }
